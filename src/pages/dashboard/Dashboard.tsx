02cfe13e14dd94f18520fd151851f06cb6d8e285
YOGASATWIKA <satwikayoga3@gmail.com>
2025-09-12 10:19:23 +0800

    :sparkles: fixing unused import

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,5 +1,6 @@
 import PageBreadcrumb from "../../components/common/PageBreadCrumb";
 import PageMeta from "../../components/common/PageMeta";
+import { useState, useEffect } from 'react';
 
 export default function SmartEdu() {
 
--- src/services/generate/GenerateService.tsx
+++
@@ -1,1 +0,0 @@
-const API_URL = `${import.meta.env.VITE_PATH_API}base-materi/`;
