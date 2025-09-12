40585c296a68ee0b0a44c947f657ba7e1f049f54
YOGASATWIKA <satwikayoga3@gmail.com>
2025-09-12 10:11:34 +0800

    :sparkles: deploy

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,6 +1,5 @@
 import PageBreadcrumb from "../../components/common/PageBreadCrumb";
 import PageMeta from "../../components/common/PageMeta";
-import { useState, useEffect } from 'react';
 
 export default function SmartEdu() {
 
